a0d73f8828a13a9742dad656b656cb6ad8dc53b3
echo storrs <echoatday@gmail.com>
2021-11-10 10:40:55 -0500

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,32 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.wordcount = @post.body.to_s.split(" ").length
+
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
+  private
+    def post_params
+      params.require(:post).permit(:body, :wordcount)
+    end
+end
